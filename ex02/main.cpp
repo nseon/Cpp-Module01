a6a4d1bcc960d9514ccc8a09ef16691f1c445089
nseon <nseon@student.42lyon.fr>
2025-10-22 11:21:48 +0200

    fix ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,17 +6,25 @@
 /*   By: nseon <nseon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 18:47:27 by nseon             #+#    #+#             */
-/*   Updated: 2025/10/21 19:01:31 by nseon            ###   ########.fr       */
+/*   Updated: 2025/10/22 09:36:18 by nseon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
+#include <iostream>
 
 int	main()
 {
 	std::string	str = "HI THIS IS BRAIN";
 	std::string *stringPTR = &str;
-	std::string stringREF = &str;
+	std::string &stringREF = str;
 
+	std::cout << &str << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+
+	std::cout << str << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
 	return (0);
 }
